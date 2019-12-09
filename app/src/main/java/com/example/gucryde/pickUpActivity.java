54e5ab557a51f6c8a1cea218f49fa7c1f2ec122d
iDevAhmed <idevahmed@gmail.com>
2019-12-09 23:33:43 +0200

    - Added initial pick up activity

--- app/src/main/java/com/example/gucryde/pickUpActivity.java
+++ app/src/main/java/com/example/gucryde/pickUpActivity.java
@@ -8,7 +8,6 @@ public class pickUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_up);
+        
     }
 }
